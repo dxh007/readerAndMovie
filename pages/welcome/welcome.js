98a9d9c7781b6d4470e29d0d2bbcb30a95f96a5b
MR DU <d_xinhao@163.com>
2017-01-11 17:41:36 +0800

    post-detail

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,17 @@
 Page({
-    
+    onTap:function(){
+        console.log("1")
+        // wx.navigateTo({//父子级页面的跳转，跳转过去的页面作为当前页面的子页面，出现返回按钮。会触发onHide函数。父子级页面最多5级；
+        //   url: '../posts/posts'
+        // })
+        wx.redirectTo({
+          url: '../posts/posts' //作为平级页面的跳转。会触发onUnload函数。
+        })
+    },
+    onUnload:function(){
+        console.log("wx.redirectTo触发,页面被关闭卸载")
+    },
+    onHide:function(){
+        console.log("wx.navigateTo触发,父页面被隐藏")
+    }
 })
